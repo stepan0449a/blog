737230c102caa6dbf1e9d6aa6b297e442f899894
Stepan Ivanov <stepan0449@gmail.com>
2022-03-16 17:30:17 +0200

    added default index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :works
   resources :dogs
   resources :posts
+  root 'works#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
